5b454a57bb1a27ea47bdd4758a9d3ffe5d887d24
Tien Pham <tiendev94@gmail.com>
2019-09-18 19:09:59 +0300

    Summary of basic workflow

---
+++ src/locales/cs/messages.js
@@ -0,0 +1,1 @@
+/* eslint-disable */module.exports={languageData:{"plurals":function(n,ord){var s=String(n).split("."),i=s[0],v0=!s[1];if(ord)return"other";return n==1&&v0?"one":i>=2&&i<=4&&v0?"few":!v0?"many":"other"}},messages:{"Message Inbox":"P\u0159\xEDchoz\xED zpr\xE1vy"}};
